09d8586f0636b19d4833d91a37816a4031932c5c
DenisDorogov <denisdorogov@yandex.ru>
2022-06-18 09:33:35 +0500

    Added modules architectur in vuex store

--- resources/js/vuex/store.js
+++ resources/js/vuex/store.js
@@ -1,4 +1,6 @@
-import { createStore, createLogger } from 'vuex'
+import { createStore, createLogger } from 'vuex';
+import userModule from './modules/user';
+import taskModule from './modules/task';
 
 
 const debug = process.env.NODE_ENV !== 'production'
@@ -7,6 +9,8 @@ export default new createStore({
   strict: debug,
   plugins: debug ? [createLogger()] : [],
   modules: {
+    user: userModule,
+    task: taskModule
 
   },
 })
