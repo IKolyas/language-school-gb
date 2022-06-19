830a922dca650359afe6d074d0103373e3966799
kamil <k.kamil87-87@yandex.ru>
2022-06-19 11:32:34 +0500

    Added layout and header. Distributed by folders in the folder resources/js

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,19 +1,19 @@
-import {createWebHashHistory, createRouter} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 
-import Home from './components/Home';
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Practice from "./components/Practice";
+import Home from './components/pages/Home';
+import Practice from "./components/pages/Practice";
+import Login from "./components/pages/auth/Login";
+import Register from "./components/pages/auth/Register";
 
 const routes = [
     {name: 'home', path: '/', component: Home},
+    {name: 'practice', path: '/practice', component: Practice},
     {name: 'login', path: '/login', component: Login},
     {name: 'register', path: '/register', component: Register},
-    {name: 'practice', path: '/practice', component: Practice},
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes: routes,
 });
 
