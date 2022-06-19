017228cc9106dace2c5d6bed803b20c9635496e8
IKolyas <i.kolyasnikov@mail.ru>
2022-06-19 14:10:08 +0500

    app blade

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,8 +15,3 @@ mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
-mix.webpackConfig({
-    output: {
-        publicPath: '/',
-    },
-});
