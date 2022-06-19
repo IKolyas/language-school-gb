1edab444201a958b4645133ca85eacfa0c303f15
IKolyas <i.kolyasnikov@mail.ru>
2022-06-19 14:02:20 +0500

    app blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{env('APP_NAME')}}</title>
+    <base href="/" />
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
 </head>
 <body>
