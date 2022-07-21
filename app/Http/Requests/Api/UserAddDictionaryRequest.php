d0af378d67e130a7a65a93cddd82382a1f6bf9f9
Cheglok <cheglokm@yandex.ru>
2022-07-21 22:22:18 +0500

    Refactor adding dictionary to user's dictionaries. Change method from put to post, put parameters into data, create Request, create another method in UserController, that add ratings for every word in dictionary

---
+++ app/Http/Requests/Api/UserAddDictionaryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+use JetBrains\PhpStorm\ArrayShape;
+
+class UserAddDictionaryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    #[ArrayShape(['user_id' => "integer", 'dictionary_id' => "integer"])]
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|integer',
+            'dictionary_id' => 'required|integer',
+        ];
+    }
+}
