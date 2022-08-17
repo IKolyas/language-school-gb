4fb605cdcd1a93a209ec78ca74e390ea8fb04403
Cheglok <cheglokm@yandex.ru>
2022-08-18 00:47:32 +0500

    Achievements page done

--- resources/js/components/partials/AppHeader.vue
+++ resources/js/components/partials/AppHeader.vue
@@ -2,25 +2,25 @@
     <header class="main-header">
         <ul class="main-nav">
             <li class="main-nav__item">
-                <router-link tag="a" :to="{name: 'home'}" class="main-nav__link" active-class="main-nav__link_active">
+                <router-link tag="a" :to="{name: 'home'}" class="main-nav__link" active-class="main-nav__link_active bottom-marked">
                     Главная
                 </router-link>
             </li>
             <li class="main-nav__item">
                 <router-link tag="a" :to="{name: 'practiceTest'}" class="main-nav__link"
-                             active-class="main-nav__link_active">
+                             active-class="main-nav__link_active bottom-marked">
                     Практика
                 </router-link>
             </li>
             <li class="main-nav__item">
                 <router-link tag="a" :to="{name: 'achievements'}" class="main-nav__link"
-                             active-class="main-nav__link_active">
+                             active-class="main-nav__link_active bottom-marked">
                     Мои достижения
                 </router-link>
             </li>
             <li class="main-nav__item">
                 <router-link tag="a" :to="{name: 'dictionaries'}" class="main-nav__link"
-                             active-class="main-nav__link_active">
+                             active-class="main-nav__link_active bottom-marked">
                     Словари
                 </router-link>
             </li>
@@ -38,22 +38,22 @@
                 {{ name }} {{ lastname }}
             </span>
             <img class="user-menu__avatar" src="https://github.com/mdo.png" alt="mdo" width="32" height="32">
-            <div class="user-menu__dropdown-toggle"></div>
-            <ul class="user-menu__options-list" v-if="AuthMenuIsActive">
-                <li class="user-menu__options-item">
+            <div class="user-menu__dropdown-toggle dropdown-toggle-mark"></div>
+            <ul class="user-menu__options-list" v-if="authMenuIsActive">
+                <li class="user-menu__options-item bottom-marked">
                     <router-link :to="{name: 'account'}" class="user-menu__options-link">
                         Профиль
                     </router-link>
                 </li>
-                <li class="user-menu__options-item">
+                <li class="user-menu__options-item bottom-marked">
                     <!--                    TODO logout router-link-->
-                    <router-link :to="{name: 'account'}" class="user-menu__options-link">
-                        Выйти
+                    <router-link :to="{name: 'login'}" class="user-menu__options-link">
+                        Войти
                     </router-link>
                 </li>
-                <li class="user-menu__options-item">
-                    <router-link :to="{name: 'account'}" class="user-menu__options-link">
-                        Техподдержка
+                <li class="user-menu__options-item bottom-marked">
+                    <router-link :to="{name: 'register'}" class="user-menu__options-link">
+                        Регистрация
                     </router-link>
                 </li>
             </ul>
@@ -70,7 +70,7 @@ export default {
     },
     data() {
         return {
-            AuthMenuIsActive: false,
+            authMenuIsActive: false,
         }
     },
     computed: {
@@ -88,7 +88,7 @@ export default {
             fetchUser: 'user/fetchUser'
         }),
         toggleAuthMenu() {
-            this.AuthMenuIsActive = !this.AuthMenuIsActive;
+            this.authMenuIsActive = !this.authMenuIsActive;
         },
     },
 }
