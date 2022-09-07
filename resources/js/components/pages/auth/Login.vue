5b4ac61825285f7944b9d7cf4dd18d58ba87e9bf
Kamil-87 <k.kamil87-87@yandex.ru>
2022-09-07 22:18:31 +0500

    Added adaptive

--- resources/js/components/pages/auth/Login.vue
+++ resources/js/components/pages/auth/Login.vue
@@ -1,89 +1,229 @@
 <template>
-    <main class="enter-page">
-        <div class="alert alert-danger" role="alert" v-if="error !== null">
-            {{ error }}
-        </div>
-        <img src="../../../../img/main-logo.png" alt="Language school project logotype" width="555"
-             class="enter-page__logo">
-        <h1 class="enter-page__heading">Вход</h1>
-        <form class="enter-page__form enter-form" @submit.prevent="login">
-            <input type="email" class="primary-input enter-form__input enter-form__input_email" v-model="email" required
-                   autofocus autocomplete="off" placeholder="Email">
-            <input type="password" class="primary-input enter-form__input enter-form__input_password" v-model="password"
-                   required autocomplete="off" placeholder="Пароль">
-            <div class="enter-form__additions">
-                <div>
-                    <input type="checkbox" class="enter-form__checkbox" checked id="rememberMe">
-                    <label for="rememberMe" class="enter-form__text">Запомнить меня</label>
-                </div>
-                <a href="" class="enter-form__text enter-form__link">Забыли пароль?</a>
-            </div>
-            <button type="submit" class="enter-form__button">
-                Вход
-            </button>
-        </form>
-        <p class="enter-page__text">с помощью аккаунта в социальных сетях</p>
-        <ul class="enter-page__social-networks social-networks">
-            <li class="social-networks__item">
-                <a href="/socialite/auth/vk" class="social-networks__link social-networks__link_vk">VK</a>
-            </li>
-            <li class="social-networks__item">
-                <a href="" class="social-networks__link social-networks__link_ok">OK</a>
-            </li>
-            <li class="social-networks__item">
-                <a href="" class="social-networks__link social-networks__link_gmail">Gmail</a>
-            </li>
-            <li class="social-networks__item">
-                <a href="" class="social-networks__link social-networks__link_yandex">Yandex</a>
-            </li>
-            <li class="social-networks__item">
-                <a href="" class="social-networks__link social-networks__link_appstore">AppStore</a>
-            </li>
-        </ul>
-        <p class="enter-page__text">
-            Нет аккаунта?
-            <a href="" class="enter-form__link">Зарегистрируйтесь!</a>
-        </p>
+    <main class="container">
+       <div class="enter-page">
+		   <div class="alert alert-danger" role="alert" v-if="error !== null">
+			   {{ error }}
+		   </div>
+		   <div class="enter-page__inner">
+			   <div class="enter-page__img">
+				   <img src="../../../../img/main-logo.png" alt="Language school project logotype" width="555" class="enter-page__logo">
+			   </div>
+			   <div class="enter-page__content">
+				   <h1 class="enter-page__heading">Вход</h1>
+				   <form class="enter-page__form enter-form" @submit.prevent="loginHandler">
+					   <input type="email" class="primary-input enter-form__input enter-form__input_email" v-model="email" required
+							  autofocus autocomplete="off" placeholder="Email">
+					   <input type="password" class="primary-input enter-form__input enter-form__input_password" v-model="password"
+							  required autocomplete="off" placeholder="Пароль">
+					   <div class="enter-form__additions">
+						   <div>
+							   <input type="checkbox" class="enter-form__checkbox" checked id="rememberMe">
+							   <label for="rememberMe" class="enter-form__text">Запомнить меня</label>
+						   </div>
+						   <a href="" class="enter-form__text enter-form__link">Забыли пароль?</a>
+					   </div>
+
+					   <AppButton
+						   text="Войти"
+						   width="150px"
+						   height="40px"
+						   class="btn btn-outline-primary"
+						   :loading="loadingBtn"
+						   typeBtn="submit"
+					   />
+				   </form>
+				   <p class="enter-page__text">с помощью аккаунта в социальных сетях</p>
+				   <ul class="enter-page__social-networks social-networks">
+					   <li class="social-networks__item">
+						   <a href="" class="social-networks__link social-networks__link_vk">VK</a>
+					   </li>
+					   <li class="social-networks__item">
+						   <a href="" class="social-networks__link social-networks__link_ok">OK</a>
+					   </li>
+					   <li class="social-networks__item">
+						   <a href="" class="social-networks__link social-networks__link_gmail">Gmail</a>
+					   </li>
+					   <li class="social-networks__item">
+						   <a href="" class="social-networks__link social-networks__link_yandex">Yandex</a>
+					   </li>
+					   <li class="social-networks__item">
+						   <a href="" class="social-networks__link social-networks__link_appstore">AppStore</a>
+					   </li>
+				   </ul>
+				   <p class="enter-page__text">
+					   Нет аккаунта?
+					   <a href="" class="enter-form__link">Зарегистрируйтесь!</a>
+				   </p>
+			   </div>
+		   </div>
+	   </div>
     </main>
 </template>
 
 <script>
 import {mapActions} from "vuex";
+import { login } from '../../../services/auth.service'
+import AppButton from '../../partials/AppButton';
 
 export default {
     name: "Login",
-    data() {
+	components: {AppButton},
+	data() {
         return {
             email: "",
             password: "",
-            error: null
+            error: null,
+			loadingBtn: false
         }
     },
     methods: {
         ...mapActions({signIn: 'user/login'}),
-        login() {
-            this.$axios.get('/sanctum/csrf-cookie').then(() => {
-                this.$axios.post('api/user/login', {email: this.email, password: this.password})
-                    .then((response) => {
-                        if (!response.data.success) return alert(response.data.message);
+		loginHandler() {
+			login({email: this.email, password: this.password})
+			.then((response) => {
+				if (!response.data.success) return alert(response.data.message);
 
-                        this.signIn(response.data.token, response.data.user)
+				this.signIn(response.data.token, response.data.user)
 
-                        this.$store.dispatch('user/fetchUser', {id: response.data.user.id});
-
-                    })
-                    .catch((data) => {
-                        alert(data)
-                    })
-                    .finally(() => {
-                    })
-            })
-
+				this.$store.dispatch('user/fetchUser', {id: response.data.user.id});
+			})
+			.catch((data) => {
+				alert(data)
+			})
         },
     },
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+.enter-page {
+	.enter-page__inner {
+		text-align: center;
+		padding-top: 97px;
+		padding-bottom: 90px;
 
+		@media (min-width: 1400px) {
+			background-image: url("../../../../img/enter-page-background.png");
+			background-repeat: no-repeat;
+			background-position: top 100px left 50px;
+			background-size: 45%;
+
+			padding-left: 700px;
+			padding-right: 100px;
+		}
+	}
+
+	.enter-page__heading {
+		font-family: 'Raleway', sans-serif;
+		font-size: 36px;
+		color: #2A79F8;
+		margin-bottom: 20px;
+	}
+
+	.enter-page__form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: 15px;
+	}
+
+	.primary-input {
+		border: 2px solid rgba(59, 108, 233, 1);
+		height: 46px;
+		width: 522px;
+		border-radius: 20px;
+		font-family: 'Raleway', sans-serif;
+		font-size: 20px;
+		padding: 10px;
+	}
+
+	.enter-form__input {
+		margin-bottom: 40px;
+		padding-left: 50px;
+		background-repeat: no-repeat;
+		background-position: top 5px left 10px;
+	}
+
+	.primary-input:hover, .primary-input:focus {
+		border-width: 4px;
+	}
+
+	.enter-form__input_email {
+		background-image: url("../../../../img/icons/user-icon.png");
+	}
+
+	.enter-form__input_password {
+		background-image: url("../../../../img/icons/lock-icon.png");
+	}
+
+	.enter-form__additions {
+		display: flex;
+		justify-content: space-between;
+		padding-left: 20px;
+	}
+
+	.enter-form__checkbox {
+		width: 15px;
+		height: 15px;
+		margin-right: 15px;
+	}
+
+	.enter-form__text {
+		font-family: 'Raleway', sans-serif;
+		font-size: 13px;
+		font-weight: 400;
+	}
+
+	.enter-form__button {
+		height: 45px;
+		width: 94px;
+		border-radius: 10px;
+		padding: 12px 30px;
+		border: 1px solid #3B6CE9;
+		background-color: #FFF;
+	}
+
+	.enter-form__button:hover {
+		background-color: rgba(59, 108, 233, 0.12);
+	}
+
+	.enter-form__button:active {
+		background-color: rgba(59, 108, 233, 0.24);
+	}
+
+	.enter-page__social-networks {
+		margin: auto auto 40px;
+	}
+
+	.social-networks {
+		display: flex;
+		width: 350px;
+		justify-content: space-between;
+		padding: 0;
+	}
+
+	.social-networks__link {
+		display: block;
+		font-size: 0;
+		width: 50px;
+		height: 50px;
+		background-image: url("../../../../img/icons/vk-round-icon.png");
+	}
+
+	.social-networks__link_ok {
+		background-image: url("../../../../img/icons/ok-icon.png");
+	}
+
+	.social-networks__link_gmail {
+		background-image: url("../../../../img/icons/gmail-icon.png");
+	}
+
+	.social-networks__link_yandex {
+		background-image: url("../../../../img/icons/yandex-icon.png");
+	}
+
+	.social-networks__link_appstore {
+		background-image: url("../../../../img/icons/appstore-icon.png");
+	}
+}
 </style>
