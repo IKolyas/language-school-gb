3477d74e42e4df147f40297dc796ad26e722c671
Kamil-87 <k.kamil87-87@yandex.ru>
2022-09-10 20:08:16 +0500

    Added container

--- resources/js/components/pages/Dictionaries/Dictionaries.vue
+++ resources/js/components/pages/Dictionaries/Dictionaries.vue
@@ -1,19 +1,21 @@
 <template>
-    <main class="dictionaries-page">
-        <h1 class="visually-hidden">Все словари</h1>
-        <div class="dictionaries-page__search-form search-form">
-            <input v-model="search" type="search" class="search-form__input" placeholder="Поиск по словарям"/>
-        </div>
-        <router-link :to="{name: 'dictionaryCreate'}" class="dictionaries-page__new-dictionary-button">
-            Создать новый словарь
-        </router-link>
-        <ul v-if="dictionaryFilter" class="dictionaries-page__dictionaries-list dictionary-list">
-            <li v-for="dictionary in dictionaryFilter">
-                <DictionaryCard :dictionary="dictionary"
-                                class="dictionary-list__item">
-                </DictionaryCard>
-            </li>
-        </ul>
+    <main class="container">
+		<div class="dictionaries-page">
+			<h1 class="visually-hidden">Все словари</h1>
+			<div class="dictionaries-page__search-form search-form">
+				<input v-model="search" type="search" class="search-form__input" placeholder="Поиск по словарям"/>
+			</div>
+			<router-link :to="{name: 'dictionaryCreate'}" class="dictionaries-page__new-dictionary-button">
+				Создать новый словарь
+			</router-link>
+			<ul v-if="dictionaryFilter" class="dictionaries-page__dictionaries-list dictionary-list">
+				<li v-for="dictionary in dictionaryFilter">
+					<DictionaryCard :dictionary="dictionary"
+									class="dictionary-list__item">
+					</DictionaryCard>
+				</li>
+			</ul>
+		</div>
     </main>
 </template>
 
